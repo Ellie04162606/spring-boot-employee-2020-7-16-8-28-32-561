cf875623e03177d6232ff0a756da07fbeb3f9f40
Ellie04162606 <1181140808@qq.com>
2020-07-28 21:14:22 +0800

    [Company Exception][Benjamin&Ellie] feature: create company exception of add,delete,update

---
+++ src/main/java/com/thoughtworks/springbootemployee/exceptions/company/CompanyDeleteException.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.springbootemployee.exceptions.company;
+
+import com.thoughtworks.springbootemployee.exceptions.BaseException;
+
+public class CompanyDeleteException extends BaseException {
+
+    public CompanyDeleteException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/exceptions/company/CompanyUpdateException.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.springbootemployee.exceptions.company;
+
+import com.thoughtworks.springbootemployee.exceptions.BaseException;
+
+public class CompanyUpdateException extends BaseException {
+
+    public CompanyUpdateException(String message) {
+        super(message);
+    }
+}
