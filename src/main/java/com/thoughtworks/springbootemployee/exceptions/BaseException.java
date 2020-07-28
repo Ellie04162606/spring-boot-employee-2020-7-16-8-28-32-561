8a4c97d6c5b1d1f14176f01195cb4abe6761d17b
Ellie04162606 <1181140808@qq.com>
2020-07-28 20:48:04 +0800

    [Employee exception][Benjamin&Ellie] feature: create base exception

---
+++ src/main/java/com/thoughtworks/springbootemployee/exceptions/BaseException.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.springbootemployee.exceptions;
+
+public class BaseException extends Exception {
+
+    public BaseException(String message) {
+        super(message);
+    }
+}
